c74578391278b64c0ebd2eb3ea200f8696aee198
JulioSchneider1 <julio.schneider1@universo.univates.br>
2026-05-17 11:09:36 -0300

    Ajustments docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,27 +2,16 @@
 
 echo "Aguardando PostgreSQL ficar disponível..."
 
-until PGPASSWORD=postgres psql 
--h postgres 
--U postgres 
--d financeiro 
--c '\q'; do
-
-echo "PostgreSQL ainda não disponível..."
-
-sleep 2
-
+until PGPASSWORD=postgres psql -h postgres -U postgres -d financeiro -c '\q'; do
+    echo "PostgreSQL ainda não disponível..."
+    sleep 2
 done
 
 echo "PostgreSQL disponível!"
 
 echo "Executando defaultDatabase.sql..."
 
-PGPASSWORD=postgres psql 
--h postgres 
--U postgres 
--d financeiro 
--f /app/schema/defaultDatabase.sql
+PGPASSWORD=postgres psql -h postgres -U postgres -d financeiro -f /app/schema/defaultDatabase.sql
 
 echo "Inicializando aplicação Flask..."
 
