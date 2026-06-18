0605eb2ee994ec2ae31904bccf9dccd6debf503e
JulioSchneider1 <julio.schneider1@universo.univates.br>
2026-06-18 19:03:35 -0300

    Comit final 2

--- app/models/categoria.py
+++ app/models/categoria.py
@@ -1,6 +1,5 @@
 from app.models import db
 
-#Se tem um comentario muito grande, o Flake8 reclama, então vou colocar o comentário aqui em cima, para não dar erro de linha muito grande AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
 # Tabela de Categorias
 class Categoria(db.Model):
